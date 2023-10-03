e923ccbcaa8eaf97d49fd577bcb3666e61cdb9c8
xunder <va_lo5@abv.bg>
2023-10-03 16:36:56 +0300

    innital setup

---
+++ web-dog-backend/web-dog/src/main/java/com/wedog/webdog/WebDogApplication.java
@@ -0,0 +1,13 @@
+package com.wedog.webdog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebDogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebDogApplication.class, args);
+	}
+
+}
---
+++ web-dog-backend/web-dog/src/test/java/com/wedog/webdog/WebDogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wedog.webdog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebDogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
